62173167a9432071322f4bce87b50ac13aaf1179
Dineo <Dineo.Makofane@ext.shaper.co.za>
2025-07-24 10:05:52 +0200

    functions

---
+++ function.js
@@ -0,0 +1,15 @@
+//this is a void
+function person(){
+    let firstName = "Dineo";
+    let surname = "Makofane";
+    console.log(firstName, surname)
+}
+person()
+
+person ()
+function person2() {
+    let firstName = "Keneilwe";
+    let surname = "Makofane";
+    return firstName+surname;
+}
+console.log(person2())
